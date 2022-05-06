dcf08a063ed287c2c30cb2bf5d33a35cdb2198d8
Pavanarathod <edrocullen13@gmail.com>
2022-05-06 14:18:14 +0530

    setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1 className="text-6xl text-indigo-600 italic">this is twitter</h1>
+    </div>
+  );
+};
+
+export default Home;
